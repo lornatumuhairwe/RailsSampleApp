e80fc76b9e31796b5ced9f189570448052ae3ad5
Lornah Lynn Tumuhairwe <lornah.tumuhairwe@andela.com>
2017-12-27 10:07:14 +0300

    edit functionality restricted to authenticated users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+
   def show
     @user = User.find(params[:id])
   end
@@ -25,7 +27,8 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
-      # Handle updating of a user
+      flash[:success] = "Profile updated"
+      redirect_to @user
     else
       render 'edit'
     end
@@ -40,5 +43,15 @@ class UsersController < ApplicationController
       :password,
       :password_confirmation
     )
+  end
+
+  # Before filters
+
+  # Confirms a logged-in user.
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
   end
 end
