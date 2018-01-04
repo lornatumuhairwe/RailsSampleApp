db8ebe666eb9e455e96fa6687a682fc14ed8aa3b
Lornah Lynn Tumuhairwe <lornah.tumuhairwe@andela.com>
2018-01-04 19:55:18 +0300

    reset password test

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -29,7 +29,8 @@ class PasswordResetsController < ApplicationController
       render 'edit'
     elsif @user.update_attributes(user_params)
       log_in @user
-      flash[:success] = "Password has been reset."
+      @user.update_attribute(:reset_digest, nil)
+      flash[:success] = 'Password has been reset.'
       redirect_to @user
     else
       render 'edit'
