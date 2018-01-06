990231b9ff525139008f8b9f6826f3f43198a6cc
Lornah Lynn Tumuhairwe <lornah.tumuhairwe@andela.com>
2018-01-06 12:26:47 +0300

    follow tests

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -3,7 +3,7 @@ class RelationshipsController < ApplicationController
 
   def create
     @user = User.find(params[:followed_id])
-    current_user.follow(@ser)
+    current_user.follow(@user)
     respond_to do |format|
       format.html { redirect_to @user }
       format.js
@@ -18,4 +18,5 @@ class RelationshipsController < ApplicationController
       format.js
     end
   end
+
 end
